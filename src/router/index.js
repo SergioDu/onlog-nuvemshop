2f916a1e342c67fc7d15fee4a0d909263bbcb8dd
SergioDu <sergiodu@msn.com>
2024-03-18 12:11:09 -0300

    Feat: Ajustando rota para /api/v2

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,11 @@ import { deleteClient } from "../service/excluirCliente.js";
 
 export const routes = Router();
 
-routes.post('/test', (req, res) => {
+routes.post('/api/v2/test', (req, res) => {
   res.status(200).send('API FUNCIONANDO');
 })
 
-routes.get('/authorization', async (req, res) => {
+routes.get('/api/v2/authorization', async (req, res) => {
   try {
     const { code } = req.query;
     console.log(code);
@@ -22,7 +22,7 @@ routes.get('/authorization', async (req, res) => {
   }
 })
 
-routes.post('/calculoFrete', async (req, res) => {
+routes.post('/api/v2/calculoFrete', async (req, res) => {
   try {
     const responseNuvem = await converterOnlogParaNuvemResponse(req.body);
     if (responseNuvem?.status === "error") {
@@ -37,7 +37,7 @@ routes.post('/calculoFrete', async (req, res) => {
   }
 });
 
-routes.post('/pedidoPago', async (req, res) => {
+routes.post('/api/v2/pedidoPago', async (req, res) => {
   try {
     const { id, store_id } = req.body;
     await gerarEtiquetaEAtualizarPedido(id.toString().replace(" ", ""), store_id.toString().replace(" ", ""));
@@ -48,7 +48,7 @@ routes.post('/pedidoPago', async (req, res) => {
   }
 })
 
-routes.post('/excluirCliente', async (req, res) => {
+routes.post('/api/v2/excluirCliente', async (req, res) => {
   const { store_id } = req.body;
   await deleteClient(store_id);
   res.status(200).send('OK');
